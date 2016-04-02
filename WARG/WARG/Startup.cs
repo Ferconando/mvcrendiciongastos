62462a23aa58b1ddf3b13ac4b6035faadeb2aeaa
ferconando <fernando.rondon23@gmail.com>
2016-04-02 09:30:28 -0500

    Primera Paso

---
+++ WARG/WARG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WARG.Startup))]
+namespace WARG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
