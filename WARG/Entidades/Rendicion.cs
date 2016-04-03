eb1eaad5c9cfe3afd1e1afae93cd84cf362a8678
ferconando <fernando.rondon23@gmail.com>
2016-04-03 17:33:45 -0500

    Inicial Carga Entidades

---
+++ WARG/Entidades/Rendicion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entidades
+{
+    public class Rendicion
+    {
+        [Key]
+        [Required]//NOT NULL
+        public int RendicionID { get; set; }
+        //Llave Foranea
+        [Required]//NOT NULL
+        public int NumeroPLanillaID { get; set; }
+        //[MaxLength(50)]
+        [Required]//NOT NULL
+        public string FICHA_EMPLEADO { get; set; }
+        [Required]//NOT NULL
+        public DateTime FECHA_RENDICION_INI { get; set; }
+        [Required]//NOT NULL
+        public DateTime FECHA_RENDICION_FIN { get; set; }
+        //Llave Foranea       
+        [Required]//NOT NULL
+        public double FONDO_INICIAL { get; set; }
+        [Required]//NOT NULL
+        public double TOTAL_GASTADO { get; set; }
+        [Required]//NOT NULL
+        public string ESTADO_PLANILLA { get; set; }
+        [Required]//NOT NULL
+        public DateTime FECHA_CAMBIO_ESTADO { get; set; }
+        [Required]//NOT NULL
+        public DateTime FECHA_CREACION { get; set; }
+        [Required]//NOT NULL
+        public string GLOSA_GASTOS { get; set; }
+        [Required]//NOT NULL
+        public string GLOSA_MOVILIDAD { get; set; }
+        [Required]//NOT NULL
+        public string PERIODO { get; set; }
+                
+        // Creo el arreglo de Peliculas
+        public virtual List<RendicionDetalle> RendicionDetalle { get; set; }
+       // public virtual Director Director { get; set; }
+    }
+}
---
+++ WARG/Entidades/RendicionDetalle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entidades
+{
+    public class RendicionDetalle
+    {
+        [Key]
+        public int RendicionDetalleID { get; set; }
+        //[MaxLength(100)]
+        [Required]//NOT NULL
+        public int NumeroPlanilla { get; set; }
+        //[MaxLength(100)]
+        [Required]//NOT NULL
+        public DateTime FECHA_GASTO { get; set; }
+        //[MaxLength(50)]
+        [Required]//NOT NULL
+        public string CONCEPTO_ITEM { get; set; }
+        
+        public int SECUENCIA { get; set; }
+        public string TIPO_DOCTO { get; set; }
+        public string NUMERO_DOCTO { get; set; }
+        public string RAZON_SOCIAL { get; set; }
+        public string GLOSA { get; set; }
+        public double MONTO_LINEA { get; set; }
+
+        //Relacion con el padre
+        public virtual Rendicion Rendicion { get; set; }
+    }
+}
